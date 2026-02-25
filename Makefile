3b0ee0673491ec77134a4eea86430f08644d5841
Adelmar Jose Dos Santos Sanchez <addos-sa@c2r3s4.42malaga.com>
2026-02-25 11:31:55 +0100

    libreria y makefile

---
+++ Makefile
@@ -0,0 +1,53 @@
+# --- Variables ---
+NAME		= cub3D
+CC			= cc
+CFLAGS		= -Wall -Werror -Wextra 
+
+# --- Directories and Libraries ---
+SRC_DIR		= src
+OBJ_DIR		= obj
+LIBFT_DIR	= libft
+LIBFT		= $(LIBFT_DIR)/libft.a
+INCLUDES	= -I includes -I $(LIBFT_DIR)
+LIBS		= -lreadline
+
+# --- Source Files ---
+# Common source files used by both main executable and tests
+COMMON_SRCS = 
+
+# Main executable specific source
+MAIN_SRC_FILE = main.c
+
+# --- Object Files ---
+# Note: No addprefix is used here. Paths are constructed directly.
+COMMON_OBJS	= $(patsubst %.c, $(OBJ_DIR)/%.o, $(COMMON_SRCS))
+MAIN_OBJ	= $(patsubst %.c, $(OBJ_DIR)/%.o, $(MAIN_SRC_FILE))
+
+# --- Rules ---
+all: $(NAME)
+
+# Main executable
+$(NAME): $(MAIN_OBJ) $(COMMON_OBJS)
+	@make -C $(LIBFT_DIR)
+	@$(CC) $(CFLAGS) $(INCLUDES) -o $(NAME) $(MAIN_OBJ) $(COMMON_OBJS) -L$(LIBFT_DIR) -lft $(LIBS)
+	@echo "cub3D compiled!"
+
+# Generic rule to compile any .c from src to an .o in obj
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(OBJ_DIR)
+	@$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+	@echo "Compiling $<"
+
+clean:
+	@rm -rf $(OBJ_DIR)
+	@make -C $(LIBFT_DIR) clean
+	@echo "Objects cleaned."
+
+fclean: clean
+	@rm -f $(NAME)
+	@make -C $(LIBFT_DIR) fclean
+	@echo "Full clean complete."
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ cub3d.h
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: addos-sa <addos-sa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 14:44:26 by addos-sa          #+#    #+#             */
+/*   Updated: 2026/02/25 11:22:20 by addos-sa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D.H
+#define CUB3D.H
+
+# include <sys/time.h>
+# include <math.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include "./src/MLX42/include/MLX42/MLX42.h"
+# include "./src/libft/libft.h"
+# include "./src/ft_printf/ft_printf.h"
+
+typedef struct s_screen
+{
+	char    **grid;
+    int     m_width;
+    int     m_height;
+    int     rgb_floor;
+	int		rgb_ceiling
+}	t_screen;
+
+typedef struct s_point
+{
+	double	x;
+	double	y;
+}	t_point;
+
+typedef struct s_player
+{
+	t_point	position;
+	double	angle;
+}	t_player;
+
+
+typedef struct s_textures
+{
+	char			*e_path;
+	char			*n_path;
+	char			*so_path;
+	char			*w_path;
+	mlx_texture_t	*east_t;
+	mlx_texture_t	*north_t;
+	mlx_texture_t	*south_t;
+	mlx_texture_t	*west_t;
+}	t_textures;
+
+typedef struct s_cub3D
+{
+	t_screen	screen;
+	t_player	player;
+	t_textures	textures;
+	mlx_t		*mlx;
+	bool		game_running;
+	int 		image_c;
+	char		**map_copy;
+}	t_cub3D;
+
+#endif
